ddfa5493313bcdbc5bd9ce769721068543856ac2
CozmicChaz <48370067+CozmicChaz@users.noreply.github.com>
2023-08-23 12:48:12 -0400

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -5,9 +5,6 @@ import requests
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-
-
-
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -23,9 +20,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 # streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
+streamlit.header('Fruit Info') 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
