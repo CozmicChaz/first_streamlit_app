a0dfdd3a02216edf9ec83a17611edb0e1a3daedf
CozmicChaz <48370067+CozmicChaz@users.noreply.github.com>
2023-08-23 10:06:07 -0400

    Update streamli_app.py
    fixed fruits to show

--- streamli_app.py
+++ streamli_app.py
@@ -3,9 +3,6 @@ import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),default=['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-#streamlit.text(fruits_to_show)
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
@@ -16,7 +13,8 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),default=['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
