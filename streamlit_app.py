15b9c2a71ad0c847b87e5aaae06362be1a047f33
CozmicChaz <48370067+CozmicChaz@users.noreply.github.com>
2023-08-24 14:27:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ try:
   if not fruit_choice:
       streamlit.error("what fruit?")
   else:
-    back_from_function=get_fruityvice_data(fruit_choice
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function=get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.err()
 
